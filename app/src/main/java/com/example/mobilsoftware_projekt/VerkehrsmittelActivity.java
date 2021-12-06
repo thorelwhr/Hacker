d11054e9cb81b25848a9c2dc89904fbe7428f18a
Sören <jaso1012@hka.de>
2021-12-06 13:38:32 +0100

    VerkehrsmittelActivity - nicht fertig

--- app/src/main/java/com/example/mobilsoftware_projekt/VerkehrsmittelActivity.java
+++ app/src/main/java/com/example/mobilsoftware_projekt/VerkehrsmittelActivity.java
@@ -2,7 +2,16 @@ package com.example.mobilsoftware_projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class VerkehrsmittelActivity extends AppCompatActivity {
 
@@ -10,5 +19,60 @@ public class VerkehrsmittelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verkehrsmittel);
+
+        final ListView vm_listView = (ListView) findViewById(R.id.vm_listView);
+        String[] values = new String[] {"Fuß", "Fahrrad", "ÖPNV", "MIV-Fahrer", "MIV-Mitfahrer", "Sonstiges"};
+
+        final ArrayList<String> vmList = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            vmList.add(values[i]);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this, android.R.layout.simple_list_item_1, vmList);
+        vm_listView.setAdapter(adapter);
+
+        vm_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                vmList.
+
+                            }
+                        });
+            }
+
+        });
+    }
+
+
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 }
