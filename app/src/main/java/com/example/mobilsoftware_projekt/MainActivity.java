f8cd0f84919170291c579fa5334826f29ee6bcda
furo1014 <furo1014@h-ka.de>
2021-12-11 19:40:22 +0100

    Kommentierte Version

--- app/src/main/java/com/example/mobilsoftware_projekt/MainActivity.java
+++ app/src/main/java/com/example/mobilsoftware_projekt/MainActivity.java
@@ -32,23 +32,24 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-    public static final int TEXT_REQUEST = 1;
+    public static final int TEXT_REQUEST = 1; // Für Verkehrsmittelauswahl, Funktion wie bei Permission
     private boolean permissionDenied = false;
 
     private GoogleMap map;
     private FloatingActionButton mTracking;
     private FloatingActionButton mVerkehrsmittel;
 
-    //shared preferences
+    /*shared preferences
     private String lastMapStyle;
     private String restoredMapStyle;
     private boolean trafficEnabled = false;
     private boolean indoorEnabled = false;
-    private boolean buildingEnabled = false;
+    private boolean buildingEnabled = false;*/
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -58,28 +59,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             requestFinePermission();
         }*/
         //retrieve settings
-        SharedPreferences settings;
+
+        /*SharedPreferences settings;
         settings = getSharedPreferences("SAVE_MAP_SETTINGS", Context.MODE_PRIVATE);
         restoredMapStyle = settings.getString("STYLE_OF_MAP", "MAP_TYPE_NORMAL)");
         trafficEnabled = settings.getBoolean("TRAFFIC_SHOWING_ON_MAP", false);
         buildingEnabled = settings.getBoolean("BUILDINGS_SHOWING_ON_MAP", false);
         indoorEnabled =settings.getBoolean("INDOOR_SHOWING_ON_MAP", false);
-        lastMapStyle = restoredMapStyle;
+        lastMapStyle = restoredMapStyle;*/
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         mTracking = (FloatingActionButton) findViewById(R.id.fab_tracking);
         mVerkehrsmittel = (FloatingActionButton)  findViewById(R.id.fab_verkehrsmittel);
-
-        /*Intent intent = getIntent();
-        String verkehrsmittel = intent.getStringExtra(VerkehrsmittelActivity.EXTRA_VM);
-        if (verkehrsmittel == null){
-            //do nothing
-        }
-        else {
-            Toast.makeText(this, verkehrsmittel, Toast.LENGTH_SHORT).show();
-        }*/
 
         mTracking.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,7 +93,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        if(restoredMapStyle == null) {
+        // davor gespeicherte KArteneinstellungen werden hier wieder aufgerufen
+
+        /*if(restoredMapStyle == null) {
             map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         } else if (restoredMapStyle.equals("MAP_TYPE_NORMAL")) {
             map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
@@ -121,7 +116,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         if (indoorEnabled == true){
             map.setBuildingsEnabled(true);
-        }
+        }*/
 
         /*if ((ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED))
         {
@@ -132,27 +127,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         enableMyLocation();
     }
 
-    private void enableMyLocation() {
+    private void enableMyLocation()
+    {
         if ((ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)){
-            if (map != null){
+            if (map != null) // Für Null-Pointer-Exception
+            {
                 map.setMyLocationEnabled(true);
             }
         }
-        else{
+        else
+            {
             PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
                     Manifest.permission.ACCESS_FINE_LOCATION, true);
         }
     }
 
     @Override
-    public boolean onMyLocationButtonClick() {
+    public boolean onMyLocationButtonClick()
+    {
         Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT)
                 .show();
         return false;
     }
 
     @Override
-    public void onMyLocationClick(@NonNull Location location) {
+    public void onMyLocationClick(@NonNull Location location)
+    {
         Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
     }
 
@@ -187,45 +187,57 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) // Zugriff bereits gewährt
+        {
             return;
         }
-        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
-                Manifest.permission.ACCESS_FINE_LOCATION)) {
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults, // Zugriff wird gewährt
+                Manifest.permission.ACCESS_FINE_LOCATION))
+        {
             enableMyLocation();
-        } else {
+        } else
+            {
             permissionDenied = true;
         }
     }
 
 
     @Override
-    protected void onResumeFragments() {
+    protected void onResumeFragments()
+    {
         super.onResumeFragments();
-        if (permissionDenied) {
+        if (permissionDenied) // Wenn Zugriff verweigert wir: Error Message
+        {
             showMissingPermissionError();
             permissionDenied = false;
         }
     }
 
 
-    private void showMissingPermissionError() {
+    private void showMissingPermissionError()
+    {
         PermissionUtils.PermissionDeniedDialog
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 
     @Override
-    public void  onActivityResult(int requestCode, int resultCode, Intent data){
+    public void  onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TEXT_REQUEST){
-            if (resultCode == RESULT_OK){
+        if (requestCode == TEXT_REQUEST) // Wenn request angenommen wurde
+        {
+            if (resultCode == RESULT_OK) // Wenn ich ein Ergebnis hab
+            {
                 String verkehrsmittel = data.getStringExtra(VerkehrsmittelActivity.EXTRA_VM);
                 Toast.makeText(this, verkehrsmittel, Toast.LENGTH_SHORT).show();
                 mVerkehrsmittel = findViewById(R.id.fab_verkehrsmittel);
 
-                if (verkehrsmittel.equals(getString(R.string.vmFuß))) {
+                if (verkehrsmittel.equals(getString(R.string.vmFuß)))
+                {
                     mVerkehrsmittel.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_fussgaenger));
                 }
                 if (verkehrsmittel.equals(getString(R.string.vmFahrrad))) {
@@ -265,14 +277,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onStop() {
         super.onStop();
-        SharedPreferences settings;
+        /*SharedPreferences settings;
         settings = getApplicationContext().getSharedPreferences("SAVE_MAP_SETTINGS", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = settings.edit();
         editor.putString("STYLE_OF_MAP", lastMapStyle);
         editor.putBoolean("TRAFFIC_SHOWING_ON_MAP", trafficEnabled);
         editor.putBoolean("BUILDINGS_SHOWING_ON_MAP", buildingEnabled);
         editor.putBoolean("INDOOR_SHOWING_ON_MAP", indoorEnabled);
-        editor.apply();
+        editor.apply();*/
     }
 
     @Override
@@ -289,42 +301,48 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onDestroy() {
         super.onDestroy();
 
-        SharedPreferences settings;
+        /*SharedPreferences settings;
         settings = getApplicationContext().getSharedPreferences("SAVE_MAP_SETTINGS", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = settings.edit();
         editor.putString("STYLE_OF_MAP", lastMapStyle);
         editor.putBoolean("TRAFFIC_SHOWING_ON_MAP", trafficEnabled);
         editor.putBoolean("BUILDINGS_SHOWING_ON_MAP", buildingEnabled);
         editor.putBoolean("INDOOR_SHOWING_ON_MAP", indoorEnabled);
-        editor.apply();
+        editor.apply();*/
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.map_menu, menu);
         return true;
-    }
+    }*/
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        if (trafficEnabled == true){
+    /*@Override
+    public boolean onPrepareOptionsMenu(Menu menu)
+    {
+        if (trafficEnabled == true)
+        {
             MenuItem item = menu.findItem(R.id.traffic_switch);
             item.setIcon(R.drawable.ic_checked_mark);
         }
-        if (buildingEnabled == true){
+        if (buildingEnabled == true)
+        {
             MenuItem item = menu.findItem(R.id.building_switch);
             item.setIcon(R.drawable.ic_checked_mark);
         }
-        if (indoorEnabled == true){
+        if (indoorEnabled == true)
+        {
             MenuItem item = menu.findItem(R.id.indoor_switch);
             item.setIcon(R.drawable.ic_checked_mark);
         }
         return super.onPrepareOptionsMenu(menu);
-    }
+    }*/
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    /*@Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
         switch (item.getItemId()){
                 case R.id.normal_map:
                     map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
@@ -343,39 +361,45 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     lastMapStyle = "MAP_TYPE_TERRAIN";
                     return true;
             case R.id.traffic_switch:
-                if(trafficEnabled){
+                if(trafficEnabled)
+                {
                     map.setTrafficEnabled(false);
                     item.setIcon(R.drawable.ic_unchecked_mark);
                     trafficEnabled = false;
                     return true;
                 }
-                else{
+                else
+                    {
                     map.setTrafficEnabled(true);
                     item.setIcon(R.drawable.ic_checked_mark);
                     trafficEnabled = true;
                     return true;
                 }
             case R.id.building_switch:
-                if(buildingEnabled){
+                if(buildingEnabled)
+                {
                     map.setBuildingsEnabled(false);
                     item.setIcon(R.drawable.ic_unchecked_mark);
                     buildingEnabled = false;
                     return true;
                 }
-                else{
+                else
+                    {
                     map.setBuildingsEnabled(true);
                     item.setIcon(R.drawable.ic_checked_mark);
                     buildingEnabled = true;
                     return true;
                 }
             case R.id.indoor_switch:
-                if(indoorEnabled){
+                if(indoorEnabled)
+                {
                     map.setIndoorEnabled(false);
                     item.setIcon(R.drawable.ic_unchecked_mark);
                     indoorEnabled = false;
                     return true;
                 }
-                else{
+                else
+                    {
                     map.setIndoorEnabled(true);
                     item.setIcon(R.drawable.ic_checked_mark);
                     indoorEnabled = true;
@@ -387,5 +411,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
+    }*/
 }
