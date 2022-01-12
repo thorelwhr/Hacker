7950de7f9a52f8d395914e4e38a7ac898e8a86da
furo1014 <furo1014@h-ka.de>
2022-01-12 15:32:35 +0100

    Zweite Teilaufgabe gemacht: Mini-Veränderung

---
+++ app/src/main/java/com/example/mobilsoftware_projekt/ImageButtonActivity.java
@@ -0,0 +1,24 @@
+package com.example.mobilsoftware_projekt;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class ImageButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_button);
+
+        Toolbar toolbar =findViewById(R.id.toolbar_fuer_ImageButtonActivity);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Trackdaten");
+
+        ActionBar zuruck = getSupportActionBar();
+        zuruck.setDisplayHomeAsUpEnabled(true);
+    }
+}
