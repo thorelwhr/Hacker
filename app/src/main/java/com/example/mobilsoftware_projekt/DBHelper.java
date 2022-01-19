c7c0f7fc49a22bfd8a97dfc551a71fcc21c40f62
furo1014 <furo1014@h-ka.de>
2022-01-19 16:08:10 +0100

    Debugging Aufgabe 4

--- app/src/main/java/com/example/mobilsoftware_projekt/DBHelper.java
+++ app/src/main/java/com/example/mobilsoftware_projekt/DBHelper.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.ContentValues;
@@ -25,18 +26,16 @@ public class DBHelper extends SQLiteOpenHelper
     public static final String COLUMN_NAME_ZEIT = "Zeit";
     public static final String COLUMN_NAME_DISTANZ = "Distanz";
     public static final String COLUMN_NAME_DATUM = "Datum";
-    public static final String COLUMN_NAME_STANDORT = "Standorte";
+    public static final String COLUMN_NAME_STANDORT = "Standort";
 
     public static final String CreateDatabase = "CREATE TABLE "+ TABLE_NAME+ "("+  COLUMN_NAME_ID+ "INTEGER PRIMARY KEY AUTOINCREMENT, "+
             COLUMN_NAME_VERKEHRSMITTEL+ "TEXT NOT NULL, "+ COLUMN_NAME_ZEIT+ "TEXT NOT NULL, "+ COLUMN_NAME_DISTANZ+ "TEXT NOT NULL, "+
             COLUMN_NAME_DATUM+ "TEXT NOT NULL, "+ COLUMN_NAME_STANDORT+ "TEXT NOT NULL);";
 
 
+    private static final String _DB_FILE_NAME = "locations.db";
+    public DBHelper(@Nullable Context context){super(context, _DB_FILE_NAME, null, 1 );}
 
-    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version)
-    {
-        super(context, name, factory, version);
-    }
 
     @Override
     public void onCreate(SQLiteDatabase db)
@@ -58,15 +57,25 @@ public class DBHelper extends SQLiteOpenHelper
     contentValues.put(COLUMN_NAME_ID, ID);
     contentValues.put(COLUMN_NAME_VERKEHRSMITTEL, Verkehrsmittel);
     contentValues.put(COLUMN_NAME_DISTANZ, Distanz);
-        contentValues.put(COLUMN_NAME_DATUM, Datum);
-        contentValues.put(COLUMN_NAME_STANDORT, Standort);
+    contentValues.put(COLUMN_NAME_DATUM, Datum);
+    contentValues.put(COLUMN_NAME_STANDORT, Standort);
 
-        long result = db.insert(TABLE_NAME, null, contentValues);
+    long result = db.insert(TABLE_NAME, null, contentValues);
 
-        if (result == -1) {
-            return false;
-        } else {
+    if (result == -1)
+    {
+        return false;
+    }
+    else {
             return true;
         }
+    }
+
+    public Cursor getData()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor data = db.rawQuery(query,null);
+        return data;
     }
 }
--- app/src/main/java/com/example/mobilsoftware_projekt/ImageButtonActivity.java
+++ app/src/main/java/com/example/mobilsoftware_projekt/ImageButtonActivity.java
@@ -4,9 +4,20 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class ImageButtonActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ImageButtonActivity extends AppCompatActivity
+{
+
+    ArrayList<String> dataList;
+    ListView listview = findViewById(R.id.ListView);
+    DBHelper mDBHelper;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -14,11 +25,24 @@ public class ImageButtonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_button);
 
+        mDBHelper = new DBHelper(this);
+
         Toolbar toolbar =findViewById(R.id.toolbar_fuer_ImageButtonActivity);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Trackdaten");
 
+
+
+
         ActionBar zuruck = getSupportActionBar();
         zuruck.setDisplayHomeAsUpEnabled(true);
+
+        getDatata();
+    }
+
+    protected void getDatata()
+    {
+
+
     }
 }
