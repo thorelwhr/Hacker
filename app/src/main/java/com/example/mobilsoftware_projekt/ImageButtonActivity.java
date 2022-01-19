14f9bb4d98a3cab1017826a45c49c169f936e33c
furo1014 <furo1014@h-ka.de>
2022-01-19 16:48:09 +0100

    Aufgabe4d) angefangen

--- app/src/main/java/com/example/mobilsoftware_projekt/ImageButtonActivity.java
+++ app/src/main/java/com/example/mobilsoftware_projekt/ImageButtonActivity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class ImageButtonActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ImageButtonActivity extends AppCompatActivity
+{
+    ArrayList<String> dataList;
+    ListView listview = findViewById(R.id.ListView);
+    DBHelper mDBHelper = new DBHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -14,11 +22,36 @@ public class ImageButtonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_button);
 
-        Toolbar toolbar = findViewById(R.id.toolbar_fuer_ImageButtonActivity);
+
+
+        Toolbar toolbar =findViewById(R.id.toolbar_fuer_ImageButtonActivity);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Trackdaten");
 
+
+
+
         ActionBar zuruck = getSupportActionBar();
         zuruck.setDisplayHomeAsUpEnabled(true);
+
+        getData();
+    }
+
+    protected void getData()
+    {
+        if(mDBHelper != null)
+        {
+            Cursor data = mDBHelper.getData();
+            listview = new ListView(this);
+
+            while()
+            {
+
+            }
+
+        }
+
+
+
     }
 }
